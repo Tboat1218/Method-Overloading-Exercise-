713b940337a75fcfcb9a26bc044e9a868d4c2d58
Theo Boateng <Theoboateng2002@gmail.com>
2025-09-23 19:52:43 -0400

    Created add methods with overloads

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public int Add(int number1, int number2)
         {
-            Console.WriteLine("Hello, World!");
+            return number1 + number2;
         }
+        public decimal Add(decimal number1, decimal number2)
+        {
+            return number1 + number2;
+        }
+        public string Add(int number1, int number2, bool includeDollars)
+        {
+            int sum = number1 + number2;
+
+            if (includeDollars)
+            {
+                // Handle singular/plural grammar
+                if (sum == 1)
+                {
+                    return $"{sum} dollar";
+                }
+                else
+                {
+                    return $"{sum} dollars";
+                }
+            }
+
+            // If includeDollars is false, just return the sum as a string
+            return sum.ToString();
+        }
+
+
+
     }
 }
